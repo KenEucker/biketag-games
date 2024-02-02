a0bb3af5e3cdf16919c91a54a6f1b3d1c7384aa2
Daniel0n0 <daniel2002emb@gmail.com>
2024-02-02 13:49:56 -0500

    "projectId" and "dataset" were updated

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'x37ikhvs',
-    dataset: 'production',
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_PROJECT_DATASET,
   }
 })
